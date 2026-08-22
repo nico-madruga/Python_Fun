d965d83435ecabafe20d5ee8ee725b09eb397275
nico-madruga <nizinhoms@gmail.com>
2026-08-22 10:31:37 -0300

    feat: Server script made, awaits for connection

--- MadrugaScanner/scanner.py
+++ MadrugaScanner/scanner.py
@@ -1,7 +1,20 @@
 import socket
 
-HOST = "127.0.0.1"
+HOST = "0.0.0.0"
 PORT = 8080
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind((HOST, PORT))
+    server_socket.listen()
+
+    print("Waiting for Connection...")
+    conn, addr = server_socket.accept()
+
+    with conn:
+        print(f"Connected with {addr}")
+        while(True):
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+        
